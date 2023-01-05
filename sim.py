5bfb8742ebc7d7cac45f72b726548f31729ec108
Chris Wood <wchristopherwood912@gmail.com>
2023-01-04 19:35:15 -0500

    formatted output for easier display on webpage

--- sim.py
+++ sim.py
@@ -6,7 +6,7 @@ class Sim():
     def __init__(self, start_step, deadline, change_events):
         self.__start_step = start_step
         self.deadline = deadline
-        self.__change_events = change_events
+        self.__change_events = change_events #TODO implement change events
 
         self.time_remaining = deadline
 
@@ -14,17 +14,42 @@ class Sim():
         self._current_cost = output['cost']
         self._current_time = output['time']
 
+    def get_start(self):
+        return self.__start_step
+
+    def _get_steps_dict(self, step):
+        steps = {f"{step.get_step_num}":step}
+
+        for next_step in step.get_next():
+            output = self._get_steps_dict(next_step)
+            steps.update(output)
+
+        return steps
+
+    def get_json(self, step, step_num):
+        results = {}
+
+        steps = self._get_steps_dict(step)
+
+        for step in steps.keys():
+            step_json = steps[step].get_json()
+            results[f'{step_json["step"]}'] = step_json
+
+        return results
+
     def _calc(self, step, step_num):
         time = 0
         cost = 0
         step_num += 1
 
+       #if not last step
         if len(step.get_next()) > 0:
+            #recursively gets all steps and set time and cost
             for next_step in step.get_next():
                 output = self._calc(next_step, step_num)
                 time = output['time'] + step.get_time()
                 cost = output['cost'] + step.get_cost()
-    
+        #if this is the last step set time and cost
         else:
             time = step.get_time()
             cost = step.get_cost()
@@ -32,6 +57,7 @@ class Sim():
             
 
         return {"time":time, "cost":cost, "step":step_num}
+
     
     def run(self):
         current_day = 0
@@ -40,7 +66,6 @@ class Sim():
             output = self._calc(self.__start_step, 0)
             self._current_cost = output['cost']
             self._current_time = output['time']
-            # time.sleep(2)
             self.time_remaining = (current_day - self._current_time) * -1
 
             print(f"day: {current_day}")
@@ -50,13 +75,12 @@ class Sim():
             current_day += 1
 
 
-# #testing stuff
-# stepD = Step([], 70, 1, 2, 2)
-# stepC = Step([stepD], 110, 4, 10, 1)
-# stepB = Step([], 100, 4, 10, 1)
-# stepA = Step([stepB, stepC], 200, 10, 5, 2)
+#testing stuff
+# stepD = Step([], 70, 1, [25, 30, 50])
+# stepC = Step([stepD], 110, 5, [30, 45, 50])
+# stepB = Step([], 100, 4, [30, 50, 60])
+# stepA = Step([stepB, stepC], 200, 1, [70, 90, 110, 150])
 
 # sim = Sim(stepA, 26, 0)
 
 # sim.run()
-
