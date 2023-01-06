9584a87c115aeddbbe59d583d242054d0b1a850a
Chris Wood <wchristopherwood912@gmail.com>
2023-01-06 12:43:47 -0500

    added the lines to connect the steps

--- static/js/render.js
+++ static/js/render.js
@@ -10,9 +10,7 @@ fetch("/sim")
     
 function render(sim){
 
-    console.log(sim)
-
-    for (let i = 1; i < sim['num_steps']; i++) {
+    for (let i = 1; i < sim['num_steps']+1; i++) {
         const title = document.createElement("h3");
         title.innerHTML = sim[`${i}`]["step"];
 
@@ -28,11 +26,11 @@ function render(sim){
         editButtons.setAttribute("class", "edit");
 
         const time = document.createElement("p");
-        time.innerHTML = sim[`${i}`]["time"];
+        time.innerHTML = `${sim[`${i}`]["time"]} Days`;
         time.setAttribute("class", "time")
 
         const cost = document.createElement("p");
-        cost.innerHTML = sim[`${i}`]["cost"];
+        cost.innerHTML = `$${sim[`${i}`]["cost"]}`;
         cost.setAttribute("class", "cost")
 
         const step = document.createElement("div");
@@ -48,5 +46,65 @@ function render(sim){
 
         steps.push(step);
     }
+    for(let i = 1; i < sim['num_steps']; i++){
+        from = document.getElementById(`${sim[`${i}`]["step"]}`);
+        next = sim[`${i}`]["next"];
+        for(let j=0; j < next.length; j++){
+            line = document.createElement("div")
+            line.setAttribute("class", "line")
+
+            tree.appendChild(line);
+
+            to = document.getElementById(`${next[j]["step"]}`)
+
+            adjustLine(from, to, line)
+            
+        }
+        
+    }
 
 }
+
+function adjustLine(from, to, line) {
+  var fT = from.offsetTop + from.offsetHeight / 2;
+  var tT = to.offsetTop + to.offsetHeight / 2;
+  var fL = from.offsetLeft + from.offsetWidth / 2;
+  var tL = to.offsetLeft + to.offsetWidth / 2;
+
+  var CA = Math.abs(tT - fT);
+  var CO = Math.abs(tL - fL);
+  var H = Math.sqrt(CA * CA + CO * CO);
+  var ANG = (180 / Math.PI) * Math.acos(CA / H);
+
+  if (tT > fT) {
+    var top = (tT - fT) / 2 + fT;
+  } else {
+    var top = (fT - tT) / 2 + tT;
+  }
+  if (tL > fL) {
+    var left = (tL - fL) / 2 + fL;
+  } else {
+    var left = (fL - tL) / 2 + tL;
+  }
+
+  if (
+    (fT < tT && fL < tL) ||
+    (tT < fT && tL < fL) ||
+    (fT > tT && fL > tL) ||
+    (tT > fT && tL > fL)
+  ) {
+    ANG *= -1;
+  }
+  top -= H / 2;
+
+  line.style["-webkit-transform"] = "rotate(" + ANG + "deg)";
+  line.style["-moz-transform"] = "rotate(" + ANG + "deg)";
+  line.style["-ms-transform"] = "rotate(" + ANG + "deg)";
+  line.style["-o-transform"] = "rotate(" + ANG + "deg)";
+  line.style["-transform"] = "rotate(" + ANG + "deg)";
+  line.style.top = top + "px";
+  line.style.left = left + "px";
+  line.style.height = H + "px";
+}
+
+
